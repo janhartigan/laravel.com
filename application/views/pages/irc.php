3938f759224feda062e74cae5ad6852f3a1c789a
Eric Barnes <eric@ericlbarnes.com>
2012-02-23 12:48:41 -0500

    Added a paragraph about connecting to irc

--- application/views/pages/irc.php
+++ application/views/pages/irc.php
@@ -8,6 +8,8 @@
 
 					<h1>#Laravel IRC</h1>
 
+					<p>Join #laravel on irc.freenode.net in your favorite irc client or use your web browser below:</p>
+
 					<iframe src="http://webchat.freenode.net?randomnick=1&channels=laravel&prompt=1&uio=MTE9MTg1ff" width="100%" height="400"></iframe>
 				</div>
 			</div>
