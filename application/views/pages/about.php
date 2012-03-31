50c75ba25422c15aded193b506c4154b9324cf73
Taylor Otwell <taylorotwell@gmail.com>
2012-03-30 22:05:53 -0500

    added donation section to about page.

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -43,6 +43,24 @@
 						</div>
 					</div>
 
+					<h2>Donations</h2>
+
+					<p>Donations may be sent via PayPal to <?php echo HTML::email('taylorotwell@gmail.com'); ?>.</p>
+
+					<p>
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_donations">
+<input type="hidden" name="business" value="taylorotwell@gmail.com">
+<input type="hidden" name="lc" value="US">
+<input type="hidden" name="item_name" value="Laravel">
+<input type="hidden" name="no_note" value="0">
+<input type="hidden" name="currency_code" value="USD">
+<input type="hidden" name="bn" value="PP-DonationsBF:btn_donate_SM.gif:NonHostedGuest">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+					</p>
+
 					<h2>Laravel's License</h2>
 
 					<p>Laravel is open-sourced software licensed under the <a href="http://www.opensource.org/licenses/mit-license.php">MIT License</a>.</p>
